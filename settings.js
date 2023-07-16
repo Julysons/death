2656803f8f52ba0a2f815798dbae84934fbb56b1
Julysons <108318856+Julysons@users.noreply.github.com>
2023-07-16 15:15:41 -0600

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,27 +21,27 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
+global.owner = ['50245369173'] //ur owner number
+global.ownernomer = "50254369173" //ur owner number2
+global.ownername = "Eddie cohen 🎃" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.socialm = "edcolling7" //ur github or insta name
+global.location = "Guatemala, Chernlbyl" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
-global.ownerweb = "https://youtube.com/@DGXeon"
-global.websitex = "https://youtu.be/L_SIk59QeAU"
-global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
-global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
-global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
+global.botname = "BlackCore Botcore 🎃⚒"
+global.ownernumber = '50245369173'
+global.ownername = 'BlackCore 🎃☣'
+global.ownerNumber = ["50245369173@s.whatsapp.net"]
+global.ownerweb = "https://youtu.be/FtfB4OKW5xs"
+global.websitex = "https://youtu.be/FtfB4OKW5xs"
+global.wagc = "https://chat.whatsapp.com/E6mIVFdUbxTGmX6OgDrbkZ"
+global.themeemoji = '🧟‍♂️'
+global.wm = "Eddie Bot Inc."
+global.botscript = 'https://github.com/Julysons/CheemsBot-MD6' //script link
+global.packname = "Stickers By ☠"
+global.author = "BlackCore Bot exclusive deluxe 🎃⚒"
+global.creator = "50245369173@s.whatsapp.net"
 global.prefa = ['','!','.','#','&']
 global.sessionName = 'session'
 global.hituet = 0
